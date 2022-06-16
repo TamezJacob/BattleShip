da5cb0002e011fdc12d0ab70d12275be0d30928b
Bryce Harmon <bph40@txstate.edu>
2022-06-16 04:49:23 -0500

    more project structure changes

---
+++ src/main/java/GameState.java
@@ -0,0 +1,12 @@
+/**
+ * @author Danil Kolesnikov danil.kolesnikov@sjsu.edu
+ * @author Minh Phan minh.phan@sjsu.edu
+ * CS 151 HW4 Fall 2017
+ */
+
+public interface GameState {
+    //method for player1 turn to play
+    void player1Turn ();
+    //method for pflayer1 turn to play
+    void player2turn ();
+}
